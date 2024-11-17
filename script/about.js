a7b74fc77054afbe097d421e815f2acaee05306c
Guilherme Carvalho <162490839+guicarbar@users.noreply.github.com>
2024-11-16 21:07:59 -0300

    update

--- script/about.js
+++ script/about.js
@@ -1,2 +1,9 @@
-document.getElementById("about_me").onclick = function() {
-  window.open("https://www.linkedin.com/in/guilherme-desenvolvedor-fullstack/", "_blank")}
+function goContact(page) {
+  switch (page) {
+    case 1:
+      window.open("https://www.linkedin.com/in/guilherme-desenvolvedor-fullstack/", "_blank")
+      break
+    case 2:
+      window.open("https://github.com/guicarbar", "_blank")
+  }
+}
